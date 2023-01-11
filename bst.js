de6e539c95fe8bd72af7f19ae7fff8ddcf4ef5b2
Krzysztof <ki.zwolak@gmail.com>
2023-01-11 20:38:30 +0100

    Add insert

--- bst.js
+++ bst.js
@@ -21,7 +21,6 @@ function Tree (array) {
             }
         }
         array.sort(function(a, b) {return a - b});
-        console.log(`Sorted: ${array}`);
         return array;
     }
 
@@ -30,7 +29,6 @@ function Tree (array) {
     function buildTree (array, start, end) {
         if (start > end) return null;
         let mid = Math.floor((start + end) / 2);
-        console.log(mid);
         let node = nodeCreate(array[mid]);
         node.left = buildTree(array, start, mid - 1);
         node.right = buildTree(array, mid + 1, end);
@@ -48,13 +46,36 @@ function Tree (array) {
           prettyPrint(node.left, `${prefix}${isLeft ? '    ' : '│   '}`, true);
         }
     }
+
+    function insert (tree, value) {
+        if (tree.left === null && tree.right === null) {
+            if (tree.data < value) {
+                tree.right = nodeCreate(value);
+                return tree.right;
+            } else if (tree.data > value) {
+                tree.left = nodeCreate(value);
+                return tree.right;
+            }
+        }
+        if (tree.data === value) return;
+        if (value < tree.data) return insert(tree.left, value);
+        if (value > tree.data) return insert(tree.right, value);
+        console.log(tree);
+    }
+
+    let inserted = insert(main, 2);
+
+    // let printInserted = prettyPrint(insert(main, 2));
     
     return {
         sorted: buildTree(sorted, 0, sorted.length - 1),
-        printed: prettyPrint(main)
+        printed: prettyPrint(main),
+        // logged: mainConsole,
+        insert: inserted,
+        // printedInsert: printInserted,
     }
 }
 
 let test = Tree([1, 7, 4, 23, 8, 9, 4, 3, 5, 7, 9, 67, 6345, 324]);
 
-console.log(test.printed);
+console.log(test.insert);
